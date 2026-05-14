f0cd5e9381b86523adfd1624eecd210bdb77f03d
Nazeer <nazeer0943@gmail.com>
2026-05-14 13:17:51 +0100

    Added the more examples

---
+++ 01_Module_Introduction/helloworld.py
@@ -0,0 +1,21 @@
+# introduction
+# 1st Day of 30DaysOfPython Challange
+
+print( "Hello, World")
+print(2+3) # Addition
+print(3-2) # Subtraction
+print(3*2) # Multiplication
+print(6/2) # Division
+print(3**2) # Exponentiation
+print(3 % 2) # Modulus
+print(3 // 2) # Floor division                                                                                                              
+
+
+# checking data types
+print(type(10)) # int
+print(type(3.14)) # float
+print(type(1 + 3j)) # complex number
+print(type("Nazir")) # str
+print(type([1, 2, 3])) # list
+print(type({"name": "Nazir"})) # Dictionary
+print(type((9.8, 3.14, 2.7 ))) # set
