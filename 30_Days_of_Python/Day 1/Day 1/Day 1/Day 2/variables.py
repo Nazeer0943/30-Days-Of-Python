ac8bab4fa0080cf162bcf165e6fba2f49ef8603e
Nazeer <nazeer0943@gmail.com>
2026-05-14 15:14:13 +0100

    provided an update

---
+++ 30_Days_of_Python/Day 1/Day 1/Day 1/Day 2/variables.py
@@ -0,0 +1,14 @@
+# this is about variables, data types, bulit-in functions and type conversion
+# Exercises: Level 1
+# Day 2: 30 Days of python programming
+first_name = "Nazir" # string
+last_name = "Sani" # string
+full_name = first_name + " " + last_name # string concatenation
+country = "Nigeria" # string
+city =  "Katsina" # string
+age =  26 # int
+year = 2026 # int
+is_married  = True # bool
+is_true  = True # bool
+is_light_on = False # bool
+abba, ismail, musa = 1, 2, 3
